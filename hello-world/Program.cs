fedc045107c6a842e7444f5586cd3e811f907e11
BAITABLE\Baitable <imbaitable@gmail.com>
2017-04-18 02:13:33 +0100

    Initial commit.

---
+++ hello-world/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        // Entry point of application.
+        static void Main(string[] args)
+        {
+            // Set the title of the application.
+            Console.Title = "Hello World!";
+            // The bit which prints 'Hello World!'.
+            Console.WriteLine(Environment.NewLine +  "  Hello World!" + Environment.NewLine + "      by Baitable.");
+            // Keep the program alive so that you can read the previous message.
+            while (true) ;
+        }
+    }
+}
